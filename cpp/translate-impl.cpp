c38814ac485bff7aee2fadec166aa3b105634ba6
Keiko Nakata <keko.nakata@gmail.com>
2016-09-20 15:33:39 +0200

    translate-impl initial commit

---
+++ cpp/translate-impl.cpp
@@ -0,0 +1,97 @@
+#include "clang/Frontend/ASTConsumers.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
+
+using namespace llvm;
+using namespace clang;
+using namespace clang::tooling;
+
+static llvm::cl::OptionCategory TranslateImplOptions("translate-impl options");
+
+std::string NameOfFunctionDecl(const FunctionDecl *d) {
+    return std::string { "name" };
+}
+
+
+enum class StmtMode { semicolon, period, none };
+
+void TranslateStmt(Stmt *s, StmtMode mode) {
+}
+
+void TranslateFunctionDecl(const FunctionDecl *d) {
+    std::string fname = NameOfFunctionDecl(d);
+
+    if (!d->hasBody()) {
+        outs() << "(* " << fname << " has no body. *)\n";
+    }
+
+    outs() << "Definition " << fname << "\n";
+
+    Stmt* body = d->getBody();
+    TranslateStmt(body, StmtMode::period);
+
+}
+
+void TranslateDecl(const Decl *d) {
+    if (NamedDecl::classof(d)) {
+        if (ValueDecl::classof(d)) {
+            if (DeclaratorDecl::classof(d)) {
+                if (FunctionDecl::classof(d)) {
+                    TranslateFunctionDecl((const FunctionDecl *) d);
+                } else {
+                    outs() << "TranslateDecl::DeclaratorDecl::else\n";
+                }
+            } else {
+                outs() << "TranslateDecl::ValueDecl::else\n";
+            }
+        } else {
+            outs() << "TranslateDecl::NamedDecl::else\n";
+        }
+    } else {
+        outs() << "TranslateDecl::else\n";
+    }
+}
+
+void TranslateDeclContext(const DeclContext *dc) {
+    for (auto decl : dc->decls()) {
+        TranslateDecl(decl);
+    }
+}
+
+class TranslateImplConsumer : public ASTConsumer {
+
+public:
+
+    explicit TranslateImplConsumer(CompilerInstance &_compiler, StringRef file)
+        : fname(file), compiler(_compiler)
+    {
+    }
+
+    virtual void HandleTranslationUnit(ASTContext &context) {
+        TranslateDeclContext(context.getTranslationUnitDecl());
+    }
+
+private:
+    StringRef fname;
+    CompilerInstance &compiler;
+
+};
+
+class TranslateImplAction : public ASTFrontendAction {
+    virtual std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &Compiler, StringRef file) {
+        return std::unique_ptr<ASTConsumer>(new TranslateImplConsumer(Compiler, file));
+                                            
+    }
+
+};
+
+int main(int argc, const char **argv) {
+    CommonOptionsParser op(argc, argv, TranslateImplOptions);
+    ClangTool tool(op.getCompilations(), op.getSourcePathList());
+
+    int result = tool.run(newFrontendActionFactory<TranslateImplAction>().get());
+
+    return result;
+}
